c202809070cf7e93e7c3eba6e486bcfb21ec7bba
Unknown <wiktor.muraszko@wp.pl>
2017-03-31 22:13:14 +0200

    meble
    added mebl framework, and mebleId framework

--- core/src/gamejam/Mebl.java
+++ core/src/gamejam/Mebl.java
@@ -1,13 +1,25 @@
 package gamejam;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.physics.box2d.World;
 import com.mygdx.game.objects.PhysicObject;
+import com.mygdx.game.objects.SpriteObject;
 
 public class Mebl extends PhysicObject{
 
 	public Mebl(World world, float x, float y) {
 		super(world, x, y);
 		// TODO Auto-generated constructor stub
+	}
+	
+	void set(int type) {
+		switch(type){
+		case MebleId.wiktor: {
+			addSprite(new SpriteObject())
+			.addTexture(Gdx.files.internal("data/badkogic.jpg"));
+			break;
+		}
+		}
 	}
 	
 	public void update(float delta, float vx, float vy) {
---
+++ core/src/gamejam/MebleId.java
@@ -0,0 +1,6 @@
+package gamejam;
+
+public class MebleId {
+	public static final int wiktor = 1;
+	public static final int stefan = 2;
+}
