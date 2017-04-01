ce484f2b3372c3311e698d5669dd5d73530a115f
Unknown <wiktor.muraszko@wp.pl>
2017-04-01 13:05:29 +0200

    Update Mebl

--- core/src/gamejam/Mebl.java
+++ core/src/gamejam/Mebl.java
@@ -10,7 +10,7 @@ import com.mygdx.game.objects.PhysicSpriteObject;
 import com.mygdx.game.objects.SpriteObject;
 
 public class Mebl extends PhysicObject{
-	public PhysicSpriteObject hitbox;
+	public PhysicSpriteObject hitbox,hitbox2;
 	public Mebl(World world, float x, float y, int type) {
 		super(world, x, y);
 		set(type,x,y);
@@ -20,18 +20,33 @@ public class Mebl extends PhysicObject{
 		switch(type){
 		case MebleId.wiktor: {
 			hitbox = new PhysicSpriteRect(world, this, x, y,BodyType.StaticBody);
-			((PhysicSpriteRect) hitbox).createRect(100, 100, 1, 1, 1);
-			addSprite(hitbox)
-			.addTexture(Gdx.files.internal("data/Wiktor.png"));
+			((PhysicSpriteRect) hitbox).createRect(155, 77, 1, 1, 1);
+			addSprite(hitbox);
+			//.addTexture(Gdx.files.internal("data/Wiktor1.1.png"));
+			
+			addSprite(new SpriteObject(this, 0, 20))
+			.addTexture(Gdx.files.internal("data/testy/Wiktor1.1.png"));
+			break;
+		}
+		case MebleId.stefan: {
+			hitbox = new PhysicSpriteRect(world, this, x, y,BodyType.StaticBody);
+			((PhysicSpriteRect) hitbox).createRect(155, 77, 1, 1, 1);
+			addSprite(hitbox);
+			addSprite(new SpriteObject(this, 0, -5))
+			.addTexture(Gdx.files.internal("data/testy/Stefan1.0.png"));
 			break;
 		}
 		case MebleId.pufa: {
-			hitbox = new PhysicSpriteKulka(world, this, 0, 0,BodyType.StaticBody);
+			hitbox = new PhysicSpriteKulka(world, this, x-20, y+15,BodyType.StaticBody);
 			((PhysicSpriteKulka) hitbox).createBall(30, 1, 1, 1);
-			addSprite(new SpriteObject())
-			.addTexture(Gdx.files.internal("data/pufa.png"));
+			hitbox2 = new PhysicSpriteKulka(world, this, x+25, y-18,BodyType.StaticBody);
+			((PhysicSpriteKulka) hitbox2).createBall(25, 1, 1, 1);
+			addSprite(hitbox);
+			addSprite(new SpriteObject(this, 0, 0))
+			.addTexture(Gdx.files.internal("data/testy/pufa1.0.png"));
 			break;
 		}
+
 		}
 	
 	}
