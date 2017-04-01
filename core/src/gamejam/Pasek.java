81b0de4c5208d9e13264785cdda91cdbd58d4cd8
cyngiel <pstefanski07@gmail.com>
2017-04-01 21:15:31 +0200

    frajerzy

--- core/src/gamejam/Pasek.java
+++ core/src/gamejam/Pasek.java
@@ -4,17 +4,22 @@ import com.badlogic.gdx.Gdx;
 import com.mygdx.game.objects.SpriteObject;
 
 public class Pasek extends Sprajt{
-	SpriteObject pasek;
-	public Pasek(float x, float y) {
-		super(x, y);
+	SpriteObject pasek = new SpriteObject();
+
+	public Pasek() {
+		super(0, 0);
 		
 		addSprite(pasek)
-		.addTexture(Gdx.files.internal("data/frajerzy0.jpg"))
-		.addTexture(Gdx.files.internal("data/frajerzy1.jpg"))
-		.addTexture(Gdx.files.internal("data/frajerzy2.jpg"))
-		.addTexture(Gdx.files.internal("data/frajerzy3.jpg"))
+		.addTexture(Gdx.files.internal("data/frajerzy0.png"))
+		.addTexture(Gdx.files.internal("data/frajerzy1.png"))
+		.addTexture(Gdx.files.internal("data/frajerzy2.png"))
+		.addTexture(Gdx.files.internal("data/frajerzy3.png"))
 		.isVisible = true;
+		pasek.position.x = 960;
+		pasek.position.y = 540;
+		objectViewPriority = 2;
 		pasek.frameNum = 0;
+		pasek.frameTime = 99999;
 		
 	}	
 		
